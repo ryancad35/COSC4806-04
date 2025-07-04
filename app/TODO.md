1c215937b01f8d4032b35e71e5a355e8adb27f3f
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-04 21:35:21 +0000

    Removed bootstrap from home/index.php

--- app/TODO.md
+++ app/TODO.md
@@ -1,3 +1,5 @@
 # To Do
 
-- Confirm whether constructor to do auth check should be in controller or in the model for reminders
+[x] Confirm whether constructor to do auth check should be in controller or in the model for reminders
+    - Should be in controller
+[]
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,18 +1,11 @@
-<?php require_once 'app/views/templates/header.php' ?>
-<div class="container">
-    <div class="page-header" id="banner">
-        <div class="row">
-            <div class="col-lg-12">
-                <h1>COSC4806: Assignment #3</h1>
-                <p class="lead"> <?= date("F jS, Y"); ?></p>
-            </div>
+    <?php require_once 'app/views/templates/header.php' ?>
+    <div class="container">
+        <div class="page-header">
+            <h1>COSC4806: Assignment #4</h1>
+            <h2>By: Ryan Lachapelle</h2>
+            <p class="lead"><?= date("F jS, Y"); ?></p>
         </div>
-    </div>
 
-    <div class="row">
-        <div class="col-lg-12">
-            <p class="logout-link"><a href="/logout">Click here to logout</a></p>
-        </div>
+        <p class="logout-link"><a href="/logout">Click here to logout</a></p>
     </div>
-
     <?php require_once 'app/views/templates/footer.php' ?>
