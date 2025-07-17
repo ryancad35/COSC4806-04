4af1a61f4aae587fc7d8904d1e5e4ba4b7daece4
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-17 22:51:05 +0000

    Added logic for checking if user is not logged in and redirecting to /login

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,12 +2,19 @@
 
 class Home extends Controller {
 
+    // Redirect to login page if user is not logged in
+    public function __construct() {
+        if (!isset($_SESSION['auth']) || $_SESSION['auth'] !== true) {
+            header('Location: /login');
+            exit;
+        }
+    }
+
     public function index() {
       $user = $this->model('User');
       $data = $user->test();
-			
-	    $this->view('home/index');
-	    die;
-    }
 
+        $this->view('home/index');
+        die;
+    }
 }
