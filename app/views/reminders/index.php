7f883683ec05dbd5fa0024c370e6bed63b2ef047
Ryan <122823664+ryancad35@users.noreply.github.com>
2025-07-17 23:04:45 +0000

    Added bootstrap for hamburger menu / header / breadcrumbs

--- app/views/reminders/index.php
+++ app/views/reminders/index.php
@@ -1,8 +1,18 @@
     <?php require_once 'app/views/templates/header.php' ?>
+<div class="page-header">
     <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/home">Home</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Reminders</li>
+          </ol>
+        </nav>
+            </div>
+        </div>
         
-        <div class="page-header">
-            
+            <div class="row">
             <h1>Reminders</h1>
             <p><a href='reminders/create'>Create a reminder</a></p>
         </div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -13,31 +13,39 @@ if (!isset($_SESSION['auth'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>COSC 4806</title>
     <link rel="stylesheet" href="/app/views/templates/style.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-  <nav class="navbar">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
       <a class="navbar-brand" href="/home">COSC 4806</a>
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="/home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/about">About Me</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/reminders">Reminders</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/reminders/create">Create Reminder</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link dropdown-toggle" href="#">Account</a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="/reminders">My Reminders</a></li>
-            <li><a class="dropdown-item" href="/logout">Logout</a></li>
-          </ul>
-        </li>
-      </ul>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link" href="/home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/about">About Me</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/reminders">Reminders</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/reminders/create">Create Reminder</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Account
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="/reminders">My Reminders</a></li>
+              <li><a class="dropdown-item" href="/logout">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   </nav>
